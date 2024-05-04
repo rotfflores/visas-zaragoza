03108698bf20e7c0c869b4fb4228f2308092dbf9
rotfflores <cli0001091@alumnos.ujed.mx>
2024-05-04 13:43:33 -0600

    preloader is ended

--- js/index.js
+++ js/index.js
@@ -95,3 +95,11 @@ bannerElements.forEach(function(element) {
     });
 });
 
+
+///////////////////////////////////////////////////////////////////////////////////////////
+//loader
+var preloader = document.getElementById('preloader');
+
+window.addEventListener("load", function(){
+    preloader.style.display = 'none';
+});
